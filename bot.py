034610675cae1c94154a00428211856a271b997d
Alex Sparrow <alexsparrow5973@gmail.com>
2022-05-28 20:30:31 +0300

    get_emoji and get_cat func

--- bot.py
+++ bot.py
@@ -1,37 +1,56 @@
 #Alex Sparrow 17.05.2022
-import logging, settings, ephem
+
 from datetime import date
+from emoji import emojize
+from glob import glob
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from random import choice, randint
+import ephem, logging, settings
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
+cities_list = ['Москва', 'Архангельск', 'Калуга', 'Армавир', 
+'Рязань', 'Нижний-Новгород'] #как работать с большими списками?
+
+def get_smile():
+    smile = choice(settings.USER_EMOJI)
+    return emojize(smile, use_aliases=True)
+
 
 def greet_user(update, context):
     text = 'Вызван /start'
     print(text)
-    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+    smile = get_smile()
+    update.message.reply_text(f"Здравствуй, пользователь {smile}!")
 
 
-def game_cities(update, context):
+def talk_to_me(update, context):
+    smile = get_smile()
+    username = update.effective_user.first_name
+    text = update.message.text
+    print(text)
+    update.message.reply_text(f"Здравствуй, {username} {smile}! Ты написал: {text}")
+
+
+"""def game_cities(update, context):
     text = 'Вызван /cities'
     print(text)
-    cities_list = ['Москва', 'Архангельск', 'Калуга', 'Армавир',
-    'Рязань', 'Нижний-Новгород'] #как работать с большими списками?
-    cities_copy = cities_list[:]
-    user_text_list = update.message.text.split()
-    city = user_text_list[1].title()
-    for index in range(len(cities_copy)):
-        if city in cities_copy:
-            cities_copy.remove(city)
-        if city[-1].upper() == cities_copy[index][0]:
-            update.message.reply_text(f"{cities_copy[index]}, ваш ход")
-            cities_copy.remove(cities_copy[index])
-            # city = update.message.text.upper()
-            break
-        else:
-            update.message.reply_text("Я сдаюсь")
+    if context.args:
+        user_city = context.args[0]
+        cities_copy = cities_list[:]
+        while len(cities_copy) > 0:
+            for index in range(len(cities_copy)):
+                if user_city[-1].upper() == cities_copy[index][0]:
+                    message = f"{cities_copy[index]}, ваш ход"
+                    update.message.reply_text(message)
+                    cities_copy.remove(cities_copy[index])
+                    user_city = talk_to_me(update, context)
+                    break
+    else:
+        message = "Введите название города"
+        update.message.reply_text(message)"""
 
 
 def constellation_planet(update, context):
@@ -53,7 +72,7 @@ def get_word_count(update, context):
     word_list = update.message.text.split()
     wordcount = 0
     for word in word_list[1:]:
-        if isinstance(word, str) and word != '-':
+        if isinstance(word, str) and word != '-': #можно проверять также на входимость word в список из спецсимолов
             wordcount += 1
             try:
                 if isinstance(int(word), int):
@@ -74,21 +93,51 @@ def next_full_moon(update, context):
         f"{ephem.next_full_moon(date.today())}")
 
 
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(user_text)
+def play_random_numbers(user_number):
+    bot_number = randint(user_number-10, user_number+10)
+    if user_number > bot_number:
+        message = f"Ты загадал {user_number}, я загадал {bot_number}, ты выиграл!"
+    elif user_number == bot_number:
+        message = f"Ты загадал {user_number}, я загадал {bot_number}, ничья!"
+    else:
+        message = f"Ты загадал {user_number}, я загадал {bot_number}, я выиграл!"
+    return message
+
+
+def guess_number(update, context):
+    text = 'Вызван /guess'
+    print(text)
+    if context.args:
+        try:
+            user_number = int(context.args[0])
+            message = play_random_numbers(user_number)
+        except (TypeError, ValueError):
+            message = "Введите целое число"
+    else:
+        message = "Введите целое число"
+    update.message.reply_text(message)
+
+
+def send_cat_picture(update, context):
+    cat_photos_list = glob('images/cat*.jp*g')
+    cat_pic_filename = choice(cat_photos_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_pic_filename, 'rb'))
 
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
+
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("cities", game_cities))
+    # dp.add_handler(CommandHandler("cities", game_cities))
     dp.add_handler(CommandHandler("planet", constellation_planet))
     dp.add_handler(CommandHandler("nextfullmoon", next_full_moon))
     dp.add_handler(CommandHandler("wordcount", get_word_count))
+    dp.add_handler(CommandHandler("guess", guess_number))
+    dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
     logging.info("Bot is start")
     mybot.start_polling()
     mybot.idle()
